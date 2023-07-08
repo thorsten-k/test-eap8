0c572fd2da881ff21549abf3c3c350a06f4477dd
Thorsten-K <t.kisner@web.de>
2023-07-08 15:38:12 +0200

    testing JSF

--- src/main/java/de/kisner/eap8/web/mbean/WelcomeBean.java
+++ src/main/java/de/kisner/eap8/web/mbean/WelcomeBean.java
@@ -1,10 +1,26 @@
 package de.kisner.eap8.web.mbean;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
-//@Named @ViewScoped
+import javax.inject.Named;
+
+import jakarta.faces.view.ViewScoped;
+
+@Named @ViewScoped
 public class WelcomeBean implements Serializable
 {
 	private static final long serialVersionUID = 1L;
+	
+	private String greeting;
+	public String getGreeting() {return greeting;}
+	public void setGreeting(String greeting) {this.greeting = greeting;}
+
+	public WelcomeBean()
+	{
+		greeting = LocalDateTime.now().toString();
+		
+		System.out.println(this.getClass().getSimpleName()+" "+greeting);
+	}
 	
 }
