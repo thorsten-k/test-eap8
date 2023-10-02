4ddf73ab36de74b2e9d7b3f70247ae2a6ad5970a
Thorsten-K <t.kisner@web.de>
2023-10-02 06:21:33 +0200

    hash/equals added

--- src/main/java/de/kisner/test/eap/model/DemoEntity.java
+++ src/main/java/de/kisner/test/eap/model/DemoEntity.java
@@ -2,6 +2,8 @@ package de.kisner.test.eap.model;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,26 +15,25 @@ import jakarta.persistence.Table;
 public class DemoEntity implements Serializable
 {
 	public static final long serialVersionUID=1;
-	
-	
+
+
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	public long getId() {return id;}
 	public void setId(long id) {this.id = id;}
-	
 
 	private String destination;
 	public String getDestination() {return destination;}
 	public void setDestination(String destination) {this.destination = destination;}
 	
 	
+	@Override public boolean equals(Object object){return (object instanceof DemoEntity) ? id == ((DemoEntity) object).getId() : (object == this);}
+	@Override public int hashCode() {return new HashCodeBuilder(17, 53).append(id).toHashCode();}
+
 	@Override public String toString()
 	{
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 		sb.append("[").append(id).append("]");
 		return sb.toString();
 	}
-	
-//	@Override public boolean equals(Object object) {return (object instanceof JeeBooking) ? id == ((JeeBooking) object).getId() : (object == this);}
-//	@Override public int hashCode() {return new HashCodeBuilder(23,13).append(id).toHashCode();}
 }
